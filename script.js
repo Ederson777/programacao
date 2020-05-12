1cc1bc4e1021f723223422d80784dd510388545a
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2020-05-12 15:05:57 -0300

    atualizaçao

---
+++ script.js
@@ -0,0 +1,80 @@
+// document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+
+// document.write("Algum Texto");
+
+// window.alert("Mensagem de Exemplo");
+
+// console.log("A mensagem que eu inserir aqui, aparecerá no console.");
+
+
+// var nome = "Bonieky";
+// var idade = 90;
+// alert(nome);
+
+// var x = 10;
+// var y = 15;
+
+// var total = x + y;
+
+// alert(total);
+
+// var nome = "Bonieky";
+// var idade = 90;
+
+// var total = nome + idade;
+
+// alert(total);
+// alert(idade);
+
+// /**
+//  * variáveis 
+//  * condicionais
+//  * 
+//  */
+
+// var hora = 17;
+
+// if ( hora == 12 || hora == 18 ) {
+//     console.log("Você está na hora do rush");
+// }
+
+
+// var nome = "Ederson";
+
+// console.log(window.nome );
+
+// let nome = "Ederson";
+
+// if (nome == "Ederson") {
+//     let idade = 90;
+// }
+
+// console.log(idade);
+
+// const nome = "Bonieky";
+
+// const nome = {nome:'Ederson', sobrenome:'Samuel'};
+// nome.nome = 'Pedro';
+
+// console.log(nome);
+
+// var pessoa = {nome:'Bonieky', sobrenome:'Lacerda'};
+// console.log(pessoa);
+
+// var nome = "Ederson";
+// var idade = 90;
+
+// var salvo = true;
+// var salvo = false;
+
+// let nome = "Ederson";
+// console.log(window.nome);
+
+// let nome = "Ederson";
+// console.log(nome);
+
+const nome = {nome:'Ederson', sobrenome:'Samuel'};
+
+nome.nome = 'Pedro';
+
+console.log(nome);
